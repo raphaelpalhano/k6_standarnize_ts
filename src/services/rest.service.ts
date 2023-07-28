9e603d23f1fe1fdcaefc6ba98a6fafbdd967bdbd
Raphael Angel <raphael1angel@hotmail.com>
2023-07-27 23:45:29 -0300

    finish flow automation

--- src/services/rest.service.ts
+++ src/services/rest.service.ts
@@ -1,3 +1,17 @@
-export function postHttp() {
-  
+import http from "k6/http";
+import { setupEnv } from "../envs/env.control";
+
+const ENV = setupEnv(__ENV.ENVIRONMENT ?? 'NONPROD');
+
+
+export function getHttp(endpoint: string, headers: object) {
+  const request = http.request('GET', `${ENV.API_URL}/${endpoint}`, '', headers);
+
+  return request;
+}
+
+export function postHttp(endpoint: string, headers: object) {
+  const request = http.request('GET', `${ENV.API_URL}/${endpoint}`, '', headers);
+
+  return request;
 }
--- src/tests/api/swapi/load-people.test.ts
+++ src/tests/api/swapi/load-people.test.ts
@@ -2,7 +2,7 @@ import { group } from 'k6';
 import { optionsLoad } from '../../../options/options.load';
 import getSwapi from '../../../services/swap.service';
 
-
+//options
 export const options = optionsLoad('10s', '1m', '5s', 50, 100);
 
 //setup (generate token)
@@ -19,7 +19,7 @@ export default function () {
 }
 
 
-//valid token and finish test
+//terdown token and finish test
 export function teardown(data: any): void{
   if(!data){
     throw new Error(`token not generate ${JSON.stringify(data.token)}`);
